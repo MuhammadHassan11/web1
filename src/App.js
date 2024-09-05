1a26e60464d228eb5a9c52ebab6057f6711555ee
MuhammadHassan <muhammadhaxxan6@gmail.com>
2024-09-05 13:42:15 +0500

    working frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import ProductForm from './components/ProductForm';
+import ProductList from './components/ProductList';
+import Profile from './components/Profile';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+
+        <Route path="/productlist" element={<ProductList />} />
+        <Route path="/productForm" element={<ProductForm />} />
+        <Route path="/productForm/:id" element={<ProductForm />} />
+        <Route path="/profile" element={<Profile />} />
+
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
+
